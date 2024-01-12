dec03e3f6cb5fccb5641ea06c46d56c11986f021
Shagu13 <adeishvili.sh@gmail.com>
2024-01-12 15:35:23 +0400

    added

---
+++ FinalProjectATM/Path.cs
@@ -0,0 +1,12 @@
+﻿
+namespace FinalProjectATM
+{
+    public class Path
+    {
+        public string GetPath()
+        {
+            string path = @"C:\Users\User\Desktop\Programming\COMM School\FinalProjectATM\FinalProjectATM\Data.json";
+            return path;
+        }
+    }
+}
---
+++ FinalProjectATM/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FinalProjectATM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*----------  REGISTER ACCOUNT  ----------*/
+            //var storedData = new RegisterNewClient().Register();
+            //Console.WriteLine(storedData);
+
+            var path = new Path().GetPath();
+            var brain = new Brain(path: path);
+            if (brain.CheckData())
+            {
+                if (brain.VerifyPin())
+                {
+                    Console.Clear();
+                    brain.DisplayMenu();
+                }
+            }
+
+        }
+    }
+}
